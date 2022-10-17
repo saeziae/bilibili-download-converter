e3f5918bbd2a369917495014e23d302412f7ef18
Estela ad Astra <i@estela.cn>
2022-10-17 15:35:08 +0800

    refactor renaming

--- bilibiliconverter.py
+++ bilibiliconverter.py
@@ -15,10 +15,10 @@ for root, dirs, files in os.walk("./", topdown=False):
         name = info["title"]
         if "page_data" in info:
             if "download_subtitle" in info["page_data"]:
-                name = name + info["page_data"]["download_subtitle"]
+                name = info["page_data"]["download_subtitle"]
         elif "ep" in info:
             name = info["ep"]["index"]
-        name = re.sub(r'(?=[\x20-\x7e]+)[^A-Za-z0-9]+', "_", name)
+        name = re.sub(r'[\x00-\x2f\x3a-\x40\x5b-\x60\7b\7f]+', "_", name)
         print(name)
         os.system("ffmpeg -i %s/video.m4s -i %s/audio.m4s -vcodec copy -acodec copy '%s.mp4' -loglevel quiet" %
                   (os.path.join(root, dirs[0]), os.path.join(root, dirs[0]), os.getcwd()+"/"+name))
